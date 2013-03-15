d8ff6e7a9a83124ba4bbaded7ba505832677ba7e
Marco Pessotto <melmothx@gmail.com>
2013-03-15 17:10:11 +0100

    Bumped version.

--- lib/Business/OnlinePayment/IPayment.pm
+++ lib/Business/OnlinePayment/IPayment.pm
@@ -24,11 +24,11 @@ Business::OnlinePayment::IPayment - Checkout via Ipayment Silent Mode
 
 =head1 VERSION
 
-Version 0.01
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 SYNOPSIS
