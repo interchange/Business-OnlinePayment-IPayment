60e65dc1b76f5321b3ce9730626856757f698951
Stefan Hornburg (Racke) <racke@linuxia.de>
2014-10-20 21:04:15 +0200

    Bump up version number.

--- lib/Business/OnlinePayment/IPayment.pm
+++ lib/Business/OnlinePayment/IPayment.pm
@@ -27,11 +27,11 @@ Business::OnlinePayment::IPayment - Checkout via Ipayment Silent Mode
 
 =head1 VERSION
 
-Version 0.06
+Version 0.07
 
 =cut
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 =head1 DESCRIPTION
 
